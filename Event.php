4e91b0c24516a6247ee91fed4a0b93327db8b9f4
Ahmet Faruk Bora <byybora@gmail.com>
2014-11-03 10:34:20 +0200

    Update Event.php

--- Event.php
+++ Event.php
@@ -8,11 +8,19 @@ namespace Plugin\Jquery1x;
 class Event
 {
     public static function ipBeforeController()
-    {
-        if(preg_match('/MSIE (.*?);/', $_SERVER['HTTP_USER_AGENT']) && !ipIsManagementState())
+    {        
+        if(!ipIsManagementState())
         {
-            \Ip\ServiceLocator::pageAssets()->removeJavaScript(ipFileUrl('Ip/Internal/Core/assets/ipCore/jquery.js'));
-            ipAddJs('http://code.jquery.com/jquery-1.11.1.min.js', null, 10);
+            $ieOnly = ipGetOption('Jquery1x.ieOnly');
+            
+            if(
+                ($ieOnly == "Yes" && preg_match('/MSIE (.*?);/', $_SERVER['HTTP_USER_AGENT'])) || 
+                ($ieOnly == "UnderIE9" && preg_match('/MSIE ([0-8]{1,}[\.0-8]{0,});/', $_SERVER['HTTP_USER_AGENT']))
+            )
+            {
+                \Ip\ServiceLocator::pageAssets()->removeJavaScript(ipFileUrl('Ip/Internal/Core/assets/ipCore/jquery.js'));
+                ipAddJs('http://code.jquery.com/jquery-1.11.1.min.js', null, 10);
+            } 
         }      
     }
 }
